01d888b5d6029413b92561509774460ed6f4b91d
fard442 <mrlutton@email.neit.edu>
2025-10-30 10:30:55 -0400

    comments

--- challenges.js
+++ challenges.js
@@ -1,8 +1,17 @@
 let canvas = document.getElementById("myCanvas");
 let pencil = canvas.getContext("2d");
 
+function gameLoop(){
 
+    //draw background
 
+    //draw stars
 
+    //move stars
+
+    //reset star path
+}
+
+setInterval(gameLoop, 50);
 
 
